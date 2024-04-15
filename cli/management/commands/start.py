6ad444edd6b534dda6f42da6ca511423e2d0317a
Matthiiews <matthieu.kouassi@gmail.com>
2024-04-15 12:15:39 +0200

    Custom management command to handle epic events.

---
+++ cli/management/commands/start.py
@@ -0,0 +1,42 @@
+from django.core.management import call_command
+
+from cli.utils_custom_command import EpicEventsCommand
+from cli.utils_menu import get_start_menu
+from cli.utils_messages import create_info_message
+
+
+class Command(EpicEventsCommand):
+    """
+    This script defines a class `Command` that inherits from
+    `EpicEventsCommand`. It provides the
+    start menu for choosing which model to go to next.
+    """
+    help = "Start the Epic Events program."
+    Permissions = ["SA", "SU", "MA"]
+
+    def handle(self, *args, **options):
+        """
+        Handle the command execution.
+
+        Displays the start menu and handles user input to navigate to
+        different sections
+        of the Epic Events program.
+        """
+        super().handle(*args, **options)
+
+        choice = get_start_menu("Epic Events")
+
+        if choice == 1:
+            call_command("employee")
+        elif choice == 2:
+            call_command("client")
+        elif choice == 3:
+            call_command("contract")
+        elif choice == 4:
+            call_command("event")
+        elif choice == 5:
+            return
+        elif choice == 6:
+            self.logout()
+            create_info_message("logging out...")
+            return
