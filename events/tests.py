f988b91a0a9a7403588a0905bfce1e5d56f2cf78
Matthiiews <matthieu.kouassi@gmail.com>
2024-04-22 11:06:42 +0200

    add queryset, improve performance in queryset and integrate headers.

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from accounts.tests import ModelTestCase
+
+
+class EventTestCase(ModelTestCase):
+    def test_event_creation_successfull(self):
+        self.assertEqual(self.event.contract.client, self.custom_client)
+        self.assertEqual(self.event.employee, self.employee)
+        self.assertEqual(self.event.location, self.LOCATION)
+        
+    def test_event_str(self):
+        self.assertEquals(
+            f"{self.event.name} ({self.employee.user.email})",
+            f"{self.NAME} ({self.USER_EMAIL})")
 
 # Create your tests here.
